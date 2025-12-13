8a082864142b482bd986d13f0a8dc164a02b23fd
PRATEEKRAJBHAR <prateekrajbhar3311@gmail.com>
2025-12-13 17:40:24 +0530

    Initial commit

--- src/Apis/Api.jsx
+++ src/Apis/Api.jsx
@@ -1,15 +1,29 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:3000/api/users",
+//   withCredentials: true,
+
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+
+
+
+// export default API;
+
+
 import axios from "axios";
 
+// Replace with your deployed Vercel backend URL
 const API = axios.create({
-  baseURL: "http://localhost:3000/api/users",
+  baseURL: "https://prateeks-projects-54eaba72.vercel.app/api/users",
   withCredentials: true,
-
   headers: {
     "Content-Type": "application/json",
   },
 });
-
-
-
 
 export default API;
